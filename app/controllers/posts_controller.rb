40dae12079fc20b28407904d19fbcc809d10ebcd
Kevin Huang <yellow.khaos@gmail.com>
2018-06-17 02:35:35 +0000

    automatically backed up by learn

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -8,9 +8,13 @@ class PostsController < ApplicationController
   end
 
   def update
-    @post.update(post_params)
+    if @post.valid?
+      @post.update(post_params)
 
-    redirect_to post_path(@post)
+      redirect_to post_path(@post)
+    else
+      render_template :edit
+    end
   end
 
   private
